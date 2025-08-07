8171f5295dbedc8343164923a02b33bfdf5d3ce5
Renan Domingos <rna.domingos@hotmail.com>
2025-08-06 22:08:10 -0300

    refactor: move InferSchema to a own class

--- app/utils/infer_schemas.py
+++ app/utils/infer_schemas.py
@@ -1,17 +1,20 @@
-import pandera as pa
-from app.etl import ExtractTransformLoad
-from data.sql.os_capa_sql import stmt_os_capa
+import pandera.pandas as pa
+import pandas as pd
 
-controller = ExtractTransformLoad()
-df_os_capa = controller.extract_sql(stmt_os_capa)
-
-
-schema_os_capa = pa.infer_schema(df_os_capa)
-
-path_schema = 'app/data/schemas/'
-
-with open(f'{path_schema}schema_os_capa.py', 'w', encoding='utf-8') as file:
-  file.write(str(schema_os_capa.to_script()))
-
-print(schema_os_capa)
+class InferSchema:
+    def __init__(
+          self,
+          data_frame: pd.DataFrame,
+          path_save_file: str
+        ) -> None:
+      self.data_frame = data_frame
+      self.path_save_file = path_save_file
+    
+    def makeInferSchema(self) -> pa.DataFrameSchema:
+        path_schema = self.path_save_file # Example: 'app/data/schemas/nome_arquivo.py'
+        schema = pa.infer_schema(self.data_frame)
+        with open(f'{path_schema}', 'w', encoding='utf-8') as file:
+           file.write(str(schema.to_script()))
+        
+        return schema
 
