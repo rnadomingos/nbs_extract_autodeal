fe51ca000e5fc48c778b91331c556ebfeb4f48ef
Renan Domingos <rna.domingos@hotmail.com>
2025-08-06 22:29:19 -0300

    refactor: add index file to customer service order sql

---
+++ app/data/sql/customer_service_orders/__init__.py
@@ -0,0 +1,4 @@
+from .sql_open_customer_service_orders import stmt_open_customer_service_orders
+from .sql_closed_customer_service_order import stmt_closed_customers_service_order
+
+__all__ = ["stmt_open_customer_service_orders", "stmt_closed_customers_service_order"]
